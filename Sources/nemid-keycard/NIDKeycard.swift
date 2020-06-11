6e6f5156b22ca360e01a6a32120a0e3b9ee2367d
Benjamin Horsleben <benjamin@fizkerinc.dk>
2020-06-12 00:54:19 +0200

    Added init for parsing the raw string input

--- Sources/nemid-keycard/NIDKeycard.swift
+++ Sources/nemid-keycard/NIDKeycard.swift
@@ -1,5 +1,7 @@
+import Foundation
+
 /// Represents a NemID Key card
-public struct NIDKeycard: Codable {
+public struct NIDKeycard: Codable, Equatable {
 	/// The unique key of the card itself. Example: G750159724.
 	public var id: String
 	/// The keys, with request as the dictionary key and response as the value.
@@ -22,4 +24,59 @@ public struct NIDKeycard: Codable {
 	/// - Parameter key: The key to look up.
 	/// - Returns: The matching value, or `nil` if no value was found.
 	public func callAsFunction(key: String) -> String? { keys[key] }
+}
+
+public extension NIDKeycard {
+	enum E: Error {
+		case invalidPair(String)
+	}
+
+	/// Init that parses a normalised String value.
+	/// The format comes from copying the content from DanID's site.
+	///
+	/// - Parameter string: The raw string that should be parsed.
+	/// - Returns: The parsed NIDKeycard, or `nil` if the input could not be parsed.
+	///
+	/// The format is a line first with the ID, then a header row that is ignored, followed by lines with the key-value parts
+	/// The data-lines have two tabs between key-value pairs, and a single tab between key and value.
+	///
+	/// Example:
+	/// ```
+	/// Human-readable-text: <id of card>
+	/// Human-readable header row
+	/// 1234	123456		1234	123456		1234	123456
+	/// 1234	123456		1234	123456		1234	123456
+	/// 1234	123456		1234	123456		1234	123456
+	/// 1234	123456		1234	123456		1234	123456
+	/// ```
+	init?(string raw: String) {
+		let lines = raw.trimmingCharacters(in: .whitespacesAndNewlines).split { $0.isNewline }.map { $0.trimmingCharacters(in: .whitespaces) }
+
+		guard let id = lines.first?.split(separator: ":").get(1)?.trimmingCharacters(in: .whitespaces)
+		else { return nil }
+
+		self.id = id
+
+		guard let keyValuePairs = try? lines.dropFirst(2).flatMap({ line -> [(String, String)] in
+			let pairs = line.components(separatedBy: "\t\t")
+			return try pairs.map {
+				let pair = $0.split(separator: "\t")
+				guard pair.count == 2
+				else { throw E.invalidPair($0) }
+
+				return (String(pair[0]), String(pair[1]))
+			}
+		}), !keyValuePairs.isEmpty
+		else { return nil }
+
+		self.keys = [String:String](uniqueKeysWithValues: keyValuePairs)
+	}
+}
+
+extension RandomAccessCollection {
+	func get(_ index: Index) -> Element? {
+		guard self.indices.contains(index)
+		else { return nil }
+		return self[index]
+	}
 }
--- Tests/nemid-keycardTests/NIDKeycardTests.swift
+++ Tests/nemid-keycardTests/NIDKeycardTests.swift
@@ -24,11 +24,74 @@ final class NIDKeycardTests: XCTestCase {
 		XCTAssertNil(card(key: "2341"))
 	}
 
+	func test_equals() {
+		let tests:[(expected: Bool, a: NIDKeycard, b: NIDKeycard)] = [
+			(true, NIDKeycard(id: "F123", keys: [:]), NIDKeycard(id: "F123", keys: [:])),
+			(true, NIDKeycard(id: "F123", keys: ["a":"1"]), NIDKeycard(id: "F123", keys: ["a":"1"])),
+			(false, NIDKeycard(id: "F123", keys: [:]), NIDKeycard(id: "F1234", keys: [:])),
+			(false, NIDKeycard(id: "F123", keys: ["a":"1"]), NIDKeycard(id: "F123", keys: ["a":"2"])),
+			(false, NIDKeycard(id: "F123", keys: ["a":"1"]), NIDKeycard(id: "F123", keys: ["b":"1"])),
+		]
+
+		for (expected, a, b) in tests {
+			XCTAssertEqual(a == b, expected)
+			XCTAssertEqual(b == a, expected)
+		}
+	}
+
+	func test_initWithDSL_validDSL_returnsCard() {
+		let input = """
+			Nøglekortnummer: G750159724
+			 Nøgle nr.	Nøgle		 Nøgle nr.	Nøgle		 Nøgle nr.	Nøgle		 Nøgle nr.	Nøgle
+			0063	103749		2734	413128		5147	625409		7779	946731
+			0087	972967		2797	862321		5164	160737		7991	420288
+			0090	873699		2822	439994		5213	600908		8032	089708
+			0168	940246		2833	054156		5219	618608		8124	704504
+			0203	044151		2856	879079		5230	830451		8168	727702
+			"""
+
+		let expected = NIDKeycard(id: "G750159724", keys: [
+			"0063": "103749", "2734": "413128", "5147": "625409", "7779": "946731",
+			"0087": "972967", "2797": "862321", "5164": "160737", "7991": "420288",
+			"0090": "873699", "2822": "439994", "5213": "600908", "8032": "089708",
+			"0168": "940246", "2833": "054156", "5219": "618608", "8124": "704504",
+			"0203": "044151", "2856": "879079", "5230": "830451", "8168": "727702",
+		])
+
+		let actual = NIDKeycard(string: input)
+		XCTAssertEqual(expected, actual)
+	}
+
+	func test_initWithDSL_invalidDSLs_returnsNil() {
+		let inputs:[String] = [
+			"""
+			Foo: no tabs in data rows
+			title row
+			123: 321
+			""",
+			"one row only",
+			"""
+			key: No data rows
+			title row
+			""",
+			"""
+			no key row
+			title row
+			123\t321
+			"""
+		]
+
+		for input in inputs {
+			XCTAssertNil(NIDKeycard(string: input), input)
+		}
+	}
 
 	static var allTests = [
 		("test_subscript_keyExists_returnsValue", test_subscript_keyExists_returnsValue),
 		("test_subscript_keyDoesNotExists_returnsNil", test_subscript_keyDoesNotExists_returnsNil),
 		("test_callable_keyExists_returnsValue", test_callable_keyExists_returnsValue),
 		("test_callable_keyDoesNotExists_returnsNil", test_callable_keyDoesNotExists_returnsNil),
+		("test_initWithDSL_validDSL_returnsCard", test_initWithDSL_validDSL_returnsCard),
+		("test_initWithDSL_invalidDSLs_returnsNil", test_initWithDSL_invalidDSLs_returnsNil),
 	]
 }
