be394166819541251aae63cb238b279115e7160d
Benjamin Horsleben <benjamin@fizkerinc.dk>
2022-07-15 10:41:27 +0200

    Removed unnecessary linux test harness

--- Tests/CLITests/DataURLTests.swift
+++ Tests/CLITests/DataURLTests.swift
@@ -8,8 +8,4 @@ final class DataURLTests: XCTestCase {
 
 		XCTAssertEqual(subject.description, "/some/url")
 	}
-
-	static var allTests = [
-		("test_description_fileURL_stripsTheScheme", test_description_fileURL_stripsTheScheme),
-	]
 }
--- Tests/LinuxMain.swift
+++
@@ -1,9 +0,0 @@
-import XCTest
-
-import CLITests
-import NemIDKeycardTests
-
-var tests = [XCTestCaseEntry]()
-tests += CLITests.allTests()
-tests += NemIDKeycardTests.allTests()
-XCTMain(tests)
