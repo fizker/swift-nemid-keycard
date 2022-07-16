999d02e0c3dba22793b4862fcf78b97071339bd8
Benjamin Horsleben <benjamin@fizkerinc.dk>
2022-07-16 14:32:56 +0200

    Added tests for the entire JSON structure

--- Tests/NemIDKeycardTests/IdentityTests.swift
+++ Tests/NemIDKeycardTests/IdentityTests.swift
@@ -148,6 +148,160 @@ final class IdentityTests: XCTestCase {
 		XCTAssertEqual(expected, actual)
 	}
 
+	func test__initFromDecoder__nemIDCredentialsMissing_mitIDCredentialsPresent__decodesAsExpected() throws {
+		let json = """
+		{
+		  "cpr" : "0101005143",
+		  "name" : "Thalia Nilsson",
+		  "mitIDCredentials" : {
+		    "password" : "asasas12",
+		    "username" : "foo"
+		  }
+		}
+		"""
+
+		let actual = try decode(json)
+
+		let expected = Identity(
+			name: "Thalia Nilsson",
+			cpr: "0101005143",
+			nemIDCredentials: nil,
+			mitIDCredentials: .init(username: "foo", password: "asasas12")
+		)
+
+		XCTAssertEqual(expected, actual)
+	}
+
+	func test__encodeToEncoder__nemIDCredentialsMissing_mitIDCredentialsPresent__encodesAsExpected() throws {
+		let identity = Identity(
+			name: "Thalia Nilsson",
+			cpr: "0101005143",
+			nemIDCredentials: nil,
+			mitIDCredentials: .init(username: "foo", password: "asasas12")
+		)
+
+		let actual = try encode(identity)
+
+		let expected = """
+		{
+		  "cpr" : "0101005143",
+		  "mitIDCredentials" : {
+		    "password" : "asasas12",
+		    "username" : "foo"
+		  },
+		  "name" : "Thalia Nilsson"
+		}
+		"""
+
+		XCTAssertEqual(expected, actual)
+	}
+
+	func test__initFromDecoder__nemIDCredentialsPresent_mitIDCredentialsPresent__decodesAsExpected() throws {
+		let json = """
+		{
+		  "cpr" : "0101005143",
+		  "name" : "Thalia Nilsson",
+		  "mitIDCredentials" : {
+		    "password" : "asasas12",
+		    "username" : "foo"
+		  },
+		  "nemIDCredentials" : {
+		    "id" : 778217849,
+		    "keycards" : [
+		      {
+		        "id" : "O310143093",
+		        "keys" : {
+		          "0038" : "641616",
+		          "0057" : "599373",
+		          "0093" : "929235",
+		          "9965" : "133251"
+		        }
+		      }
+		    ],
+		    "password" : "asasas12"
+		  }
+		}
+		"""
+
+		let actual = try decode(json)
+
+		let expected = Identity(
+			name: "Thalia Nilsson",
+			cpr: "0101005143",
+			nemIDCredentials: .init(
+				id: 778217849,
+				password: "asasas12",
+				keycards: [
+					.init(
+						id: "O310143093",
+						keys: [
+							"0038": "641616",
+							"0057": "599373",
+							"0093": "929235",
+							"9965": "133251",
+						]
+					)
+				]
+			),
+			mitIDCredentials: .init(username: "foo", password: "asasas12")
+		)
+
+		XCTAssertEqual(expected, actual)
+	}
+
+	func test__encodeToEncoder__nemIDCredentialsPresent_mitIDCredentialsPresent__encodesAsExpected() throws {
+		let identity = Identity(
+			name: "Thalia Nilsson",
+			cpr: "0101005143",
+			nemIDCredentials: .init(
+				id: 778217849,
+				password: "asasas12",
+				keycards: [
+					.init(
+						id: "O310143093",
+						keys: [
+							"0038": "641616",
+							"0057": "599373",
+							"0093": "929235",
+							"9965": "133251",
+						]
+					)
+				]
+			),
+			mitIDCredentials: .init(username: "foo", password: "asasas12")
+		)
+
+		let actual = try encode(identity)
+
+		let expected = """
+		{
+		  "cpr" : "0101005143",
+		  "mitIDCredentials" : {
+		    "password" : "asasas12",
+		    "username" : "foo"
+		  },
+		  "name" : "Thalia Nilsson",
+		  "nemIDCredentials" : {
+		    "id" : 778217849,
+		    "keycards" : [
+		      {
+		        "id" : "O310143093",
+		        "keys" : {
+		          "0038" : "641616",
+		          "0057" : "599373",
+		          "0093" : "929235",
+		          "9965" : "133251"
+		        }
+		      }
+		    ],
+		    "password" : "asasas12"
+		  }
+		}
+		"""
+
+		XCTAssertEqual(expected, actual)
+	}
+
 	func encode(_ identity: Identity) throws -> String {
 		let encoder = JSONEncoder()
 		encoder.outputFormatting = [ .sortedKeys, .prettyPrinted ]
